09e8f46443bb090858c91dd29fc7b577f37517a8
Abdul Basith <abm88@users.noreply.github.com>
2017-10-27 12:45:03 +0100

    History Graph created, and alot bug fixed

---
+++ app/src/main/java/com/example/abdulbasith/airquality/data/network/model/breezoMeterModels/HistoryGraphModel.java
@@ -0,0 +1,8 @@
+package com.example.abdulbasith.airquality.data.network.model.breezoMeterModels;
+
+/**
+ * Created by Abdul Basith on 26/10/2017.
+ */
+
+public class HistoryGraphModel {
+}
---
+++ app/src/main/java/com/example/abdulbasith/airquality/ui/locationList/historyGraphData/IHistoryGraphMVPPresenter.java
@@ -0,0 +1,8 @@
+package com.example.abdulbasith.airquality.ui.locationList.historyGraphData;
+
+/**
+ * Created by Abdul Basith on 26/10/2017.
+ */
+
+public interface IHistoryGraphMVPPresenter {
+}
