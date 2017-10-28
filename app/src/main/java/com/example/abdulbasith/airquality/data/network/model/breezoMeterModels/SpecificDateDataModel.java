cce60a14244a12c00da9eff8e23b1f7579954e51
Abdul Basith <abm88@users.noreply.github.com>
2017-10-28 15:47:05 +0100

    SpecificDateData implemented in Fragment, huge updates

---
+++ app/src/main/java/com/example/abdulbasith/airquality/data/network/model/breezoMeterModels/SpecificDateDataModel.java
@@ -0,0 +1,8 @@
+package com.example.abdulbasith.airquality.data.network.model.breezoMeterModels;
+
+/**
+ * Created by Abdul Basith on 28/10/2017.
+ */
+
+public class SpecificDateDataModel {
+}
---
+++ app/src/main/java/com/example/abdulbasith/airquality/ui/locationList/SpecificDateData/ISpecificDateDataMVPView.java
@@ -0,0 +1,8 @@
+package com.example.abdulbasith.airquality.ui.locationList.SpecificDateData;
+
+/**
+ * Created by Abdul Basith on 28/10/2017.
+ */
+
+public interface ISpecificDateDataMVPView {
+}
