2b1520d7e1df5b4f8d217b125159a6fb65ef6c3d
Abdul Basith <abm88@users.noreply.github.com>
2017-10-31 11:35:42 +0000

    dagger implemented

---
+++ app/src/main/java/com/example/abdulbasith/airquality/data/component/IApplicationComponent.java
@@ -0,0 +1,8 @@
+package com.example.abdulbasith.airquality.data.component;
+
+/**
+ * Created by Abdul Basith on 31/10/2017.
+ */
+
+public interface IApplicationComponent {
+}
---
+++ app/src/main/java/com/example/abdulbasith/airquality/data/module/ActivityModule.java
@@ -0,0 +1,8 @@
+package com.example.abdulbasith.airquality.data.module;
+
+/**
+ * Created by Abdul Basith on 31/10/2017.
+ */
+
+public class ActivityModule {
+}
